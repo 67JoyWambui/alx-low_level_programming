987afe50c513aa4dbb538cee70b17d42a9ffcd2c
67JoyWambui <njerijoylyn@gmail.com>
2023-09-09 15:05:24 -0700

    LOWERCASE AND UPPERCASE ALPHABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char letter;
+
+	letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
