adbd48343fd80ff583bcbba358107aa1f3e2b8da
67JoyWambui <njerijoylyn@gmail.com>
2023-09-12 14:35:51 -0700

    _PUTCHAR

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * main - the entry point
+ *
+ * Return: 0 on success
+ */
+
+int main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return(0);
+}
