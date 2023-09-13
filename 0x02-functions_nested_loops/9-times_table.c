cfd82d51bd69c9f996625f7cf3dc3b488d43f3f9
67JoyWambui <njerijoylyn@gmail.com>
2023-09-13 15:09:04 -0700

    TIMES TABLE

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	int n; /*this is the column*/
 	int product;
 
-	for (m = 0; m <=9; m++)
+	for (m = 0; m <= 9; m++)
 	{
 		for (n = 1; n <= 9; n++)
 		{
@@ -21,13 +21,12 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 
-				if (product <= 9){
+				if (product <= 9)
 
-				_putchar(' ');
-				}
+					_putchar(' ');
 			}
 
-			_putchar((product / 10) + '0');	
+			_putchar((product / 10) + '0');
 			_putchar((product % 10) + '0');
 		}
 
