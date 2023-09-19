15430917f5592f526b34cb5e036d1e341ca2c7a0
67JoyWambui <njerijoylyn@gmail.com>
2023-09-19 14:28:05 -0700

    PRINTS CHARACTERS OF STRING

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print a character starting with the first character
+ *
+ *@str: the string
+ */
+
+void puts2(char *str)
+{
+        int m = 0;
+        int n = 0;
+        char *z = str;
+        int num;
+
+        while (*z != '\0')
+        {
+                z++;
+                m++;
+        }
+        n = m - 1;
+
+        for (num = 0; num <= n; num++)
+        {
+                if (num % 2 == 0)
+                        _putchar(str[num]);
+
+        }
+        _putchar('\n');
+}
