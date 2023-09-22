8c555923623fba3b14e42858cbee899c54b62739
67JoyWambui <njerijoylyn@gmail.com>
2023-09-22 03:22:59 -0700

    REVERSED ARRAY

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,9 @@
  * @s1: the first string
  * @s2: the second string
  *
- * Rerurn: an integer
+ * Rerurn: - 0 if s1 and s2 are equal
+ *   - A positive value if s1 is greater than s2
+ *   - A negative value if s1 is less than s2
  */
 
 int _strcmp(char *s1, char *s2)
