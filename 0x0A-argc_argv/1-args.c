b66e85724a80e24eda48072619e036539263108c
67JoyWambui <njerijoylyn@gmail.com>
2023-09-29 09:06:16 -0700

    PRINT ARGC

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: the number of arguments
+ * @argv: the number of strings in the arguments
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+		return (0);
+}
