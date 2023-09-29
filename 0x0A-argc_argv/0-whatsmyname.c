3549681798cd923025983b2addc584ee464b4e8b
67JoyWambui <njerijoylyn@gmail.com>
2023-09-29 08:54:10 -0700

    MY NEW NAME

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point
+ * @argc: the number of arguments
+ *@argv: number of strings containing the arguments
+ * Return : 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	
+	printf("%s\n" , argv[0]);
+	return (0);
+}
