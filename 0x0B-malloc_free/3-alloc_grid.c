5329b1e4b4ea177c1ef514dbcdfddca06dd41fb4
67JoyWambui <njerijoylyn@gmail.com>
2023-10-04 11:49:35 -0700

    LENGTH AND WIDTH OF GRID

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function that returns a pointer to a 2
+ * dimensional array of integers
+ * @width: the width of the grid
+ * @height: the length of the grid
+ *
+ * Return: NULL if not successful
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int m, n;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	grid = (int **)malloc(sizeof(int *) * height);
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (m = 0; m < height; m++)
+	{
+		grid[m] = malloc(sizeof(int) * width);
+
+		if (grid[m] == NULL)
+		{
+			for (; m >= 0; m++)
+			{
+				free(grid[m]);
+				free(grid);
+				return (NULL);
+			}
+		}
+	}
+
+	/*initializing the elements of grid to 0*/
+	for (m = 0; m < height; m++)
+	{
+		for (n = 0; n < width; n++)
+			grid[m][n] = 0;
+	}
+
+	return (grid);
+}
