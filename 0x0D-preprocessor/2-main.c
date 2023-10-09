12723418099bbcd689a03f660b539e576dac6a32
67JoyWambui <njerijoylyn@gmail.com>
2023-10-09 07:12:04 -0700

    _FILE_

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - entry point
- * Description: this program  prints the name of the file it was 
+ * Description: this program  prints the name of the file it was
  *	compiled from, followed by a new line.
  *
  * Return: 0 on success
