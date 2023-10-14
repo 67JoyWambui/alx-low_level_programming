2ec6737e26227b1da6737f406cf80c968079eba8
67JoyWambui <njerijoylyn@gmail.com>
2023-10-14 06:13:44 -0700

    CHECK FOR INT

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - a function that searches for an integer
+ * @array: the array containing the elements
+ * @size: number of elements in the array
+ * @cmp: pointer to function
+ *
+ * Return: -1 if no elements match, otherwise 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int num;
+
+	if (size <= 0)
+		return (-1);
+
+	for (num = 0; num < size; num++)
+	{
+		if (cmp(array[num]) != 0)
+			return (num);
+	}
+
+	return (-1);
+}
