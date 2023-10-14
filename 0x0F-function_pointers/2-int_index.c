77c061eb9448ed5f7e0de4816f0f94faf012fe1d
67JoyWambui <njerijoylyn@gmail.com>
2023-10-14 06:34:38 -0700

    CHECK FOR INT

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int num;
 
-	if (size <= 0 || array == NULL)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (num = 0; num < size; num++)
