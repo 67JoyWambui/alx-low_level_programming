9f20b13124b38a90a3de868317c26938c777f81c
67JoyWambui <njerijoylyn@gmail.com>
2023-10-14 05:46:18 -0700

    ITERATE ARRAY

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,11 +13,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int m;
 
+	if (array == NULL || action == NULL)
+		return;
+
 	for (m = 0; m < size; m++)
 	{
 		action(array[m]);
 	}
-
-	if (array == NULL || action == NULL)
-		return;
 }
